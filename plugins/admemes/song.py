8fb948dbd150cb5da160d55816bd4570c76a46c2
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-11 17:20:43 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -96,47 +96,68 @@ def get_text(message: Message) -> [None,str]:
 
 
 @Client.on_message(filters.command(["video", "mp4"]))
-async def ytdl(_, message):
-    userLastDownloadTime = user_time.get(message.chat.id)
+def song(client, message):
+
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
+
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply("**ѕєαrchíng чσur ѕσng...!**")
+    ydl_opts = {"format": "bestvideo[ext=mp4]"}
     try:
-        if userLastDownloadTime > datetime.now():
-            wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
-            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
-            return
-    except:
-        pass
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+        performer = f"[ᗩᒍᗩ᙭]" 
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
 
-    url = message.text.strip()
-    await message.reply_chat_action("typing")
-    try:
-        title, thumbnail_url, formats = extractYt(url)
-
-        now = datetime.now()
-        user_time[message.chat.id] = now + \
-                                     timedelta(minutes=youtube_next_fetch)
-
-    except Exception:
-        await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+    except Exception as e:
+        m.edit(
+            "**𝙵𝙾𝚄𝙽𝙳 𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝙲𝙾𝚁𝚁𝙴𝙲𝚃 𝚃𝙷𝙴 𝚂𝙿𝙴𝙻𝙻𝙸𝙽𝙶 𝙾𝚁 𝚂𝙴𝙰𝚁𝙲𝙷 𝙰𝙽𝚈 𝙾𝚃𝙷𝙴𝚁 𝚂𝙾𝙽𝙶**"
+        )
+        print(str(e))
         return
-    buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+    m.edit("**dσwnlσαdíng чσur ѕσng...!**")
     try:
-        # Todo add webp image support in thumbnail by default not supported by pyrogram
-        # https://www.youtube.com/watch?v=lTTajzrSkCw
-        img = wget.download(thumbnail_url)
-        im = Image.open(img).convert("RGB")
-        output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
-        if not os.path.isdir(output_directory):
-            os.makedirs(output_directory)
-        thumb_image_path = f"{output_directory}.jpg"
-        im.save(thumb_image_path,"jpeg")
-        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
-        await sentm.delete()
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            Video_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        TEXT = title = results[0]["title"][:40]
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_video(video_file, caption=TEXT, parse_mode='md',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit("**🚫 𝙴𝚁𝚁𝙾𝚁 🚫**")
+        print(e)
+
+    try:
+        os.remove(video_file)
+        os.remove(thumb_name)
     except Exception as e:
         print(e)
-        try:
-            thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
-            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
-        except Exception as e:
-            await sentm.edit(
-            f"<code>{e}</code> #Error")
+
+def get_text(message: Message) -> [None,str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " not in text_to_return:
+        return None
+    try:
+        return message.text.split(None, 1)[1]
+    except IndexError:
+        return None
