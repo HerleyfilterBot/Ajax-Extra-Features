24c368b80bcc5b8b388bc166993b7dc66c3f7fae
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-11 17:55:11 +0530

    Update tts.py

--- plugins/admemes/tts.py
+++ plugins/admemes/tts.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message
 
 
 def convert(text):
-    audio = StringIO()
+    audio = Helen()
     i = Translator().translate(text, dest="en")
     lang = i.src
     tts = gTTS(text, lang=lang)
