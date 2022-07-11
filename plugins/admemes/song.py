c064dd3af75d2614d42d967f127934019cfd2c45
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-11 15:32:23 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,12 +66,13 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = #print(results)
+        #print(results)
         title = results[0]["title"][:40]
         performer = f"[ᗩᒍᗩ᙭]" 
         duration = results[0]["duration"]
-        url_suffix = results[0]["url_suffix"]
-        views = results[0]["views"]
+        views = results[0]["views"])
+
+
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
