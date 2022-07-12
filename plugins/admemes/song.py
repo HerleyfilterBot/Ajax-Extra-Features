bbab18c29d839332c88a5ae36484d4c0490c1fbc
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-12 21:09:07 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -65,8 +65,7 @@ def song(client, message):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        TEXT= title = results[0]["title"][:40
+            ydl.process_info(info_dict
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
