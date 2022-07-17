f67a40f86860a265f8e1f63de6a74674dc6d68a5
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-17 14:19:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,11 +21,9 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
-        buttons = [
-            [
-                InlineKeyboardButton('👥 ꜱᴜᴘᴘᴏʀᴛ 👥', url='https://t.me/MOVIES_LEAKED_HUB')
-            ]
-            ]
+        buttons = [[
+            InlineKeyboardButton('ꜱᴜʀᴘʀɪꜱᴇ', callback_data='deploy'),
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.PRIVATEBOT_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/Aadhi000/Ajax-Extra-Features/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
