7a08f53ac753819f22c1c5cf65ec9fc6a4aeb3b6
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-21 12:12:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -166,7 +166,23 @@ async def next_page(bot, query):
         n_offset = int(n_offset)
     except:
         n_offset = 0
-
+    if 0 < offset <= 10:
+        off_set = 0
+    elif offset == 0:
+        off_set = None
+    else:
+        off_set = offset - 10
+    if n_offset == 0:
+        btn.append(
+              [[
+              InlineKeyboardButton('⛔ ᴄʟᴏꜱᴇ', callback_data='close_data'),
+            InlineKeyboardButton('⏪ ʙᴀᴄᴋ', callback_data='source')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.PINGS_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
     if not files:
         return
     settings = await get_settings(query.message.chat.id)
