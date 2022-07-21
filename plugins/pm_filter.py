c38507135ca4c74309245ce70fe36be8339d17e4
Natya00 <106044904+Natya00@users.noreply.github.com>
2022-07-21 13:02:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -213,14 +213,9 @@ async def next_page(bot, query):
             for file in files
         ]
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
-            ]
-            for file in files
-        ]
+        btn = [[
+              InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'),
+            ]]
     if 0 < offset <= 10:
         off_set = 0
     elif offset == 0:
